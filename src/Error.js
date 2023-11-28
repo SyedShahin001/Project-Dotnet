bd76999642e893d98862119dbd95de3337741d09
SyedShahin001 <syedshahinsulthana@gmail.com>
2023-11-28 10:17:45 +0530

    fourth commit

--- src/Error.js
+++ src/Error.js
@@ -4,6 +4,7 @@ import error from "../src/Images/error.png";
 const Error = () => {
   return (
     <div>
+      
     <h1>Oops! Page Not Found.</h1>
     <p>We're sorry, but the page you requested could not be found.</p>
     <img src={error} alt="Error 404" style={{width:"100%"}}/>
